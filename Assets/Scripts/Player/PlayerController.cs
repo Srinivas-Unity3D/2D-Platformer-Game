ba1967731da41ce360457091898cb88c1231bfe3
SrinivasPrasad <43960959+SrinuSandy@users.noreply.github.com>
2025-06-09 21:20:37 +0530

    implement jump and crouch animation by user input

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -15,16 +15,34 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float speed = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Move(speed);
+        Crouch(Input.GetKey(KeyCode.LeftControl));
+        Jump(vertical > 0);
+    }
+
+    private void Move(float speed)
+    {
         playerAnimator.SetFloat("Speed", Mathf.Abs(speed));
         Vector3 scale = transform.localScale;
         if (speed < 0f)
         {
             scale.x = -1 * Mathf.Abs(scale.x);
         }
-        else if (speed > 0f) 
+        else if (speed > 0f)
         {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+    }
+
+    private void Crouch(bool isCrouch) 
+    {
+        playerAnimator.SetBool("Crouch", isCrouch);
+    }
+
+    private void Jump(bool isJump)
+    {
+        playerAnimator.SetBool("Jump", isJump);
     }
 }
