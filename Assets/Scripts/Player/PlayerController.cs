06d412afc678ac0b14f27e55184deeefebe29c81
SrinivasPrasad <43960959+SrinuSandy@users.noreply.github.com>
2025-06-10 15:32:08 +0530

    implement player movement and Jump movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,22 +6,41 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Animator playerAnimator;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private Rigidbody2D playerRB;
+    [SerializeField] private float forceSpeed;
 
     private void Start()
     {
         playerAnimator = GetComponent<Animator>();
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        float speed = Input.GetAxis("Horizontal");
+        float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Move(speed);
+
+
+        PlayerMovement(horizontal, vertical);
+        PlayerMovementAnimation(horizontal);
         Crouch(Input.GetKey(KeyCode.LeftControl));
         Jump(vertical > 0);
     }
 
-    private void Move(float speed)
+    private void PlayerMovement(float horizontal, float vertical) 
+    {
+        // player Movement
+        Vector3 playerPos = transform.position;
+        playerPos.x += horizontal * speed * Time.deltaTime;
+        transform.position = playerPos;
+
+        // Player Jump
+        playerRB.AddForce(new Vector2(0, vertical * forceSpeed), ForceMode2D.Force);
+
+    }
+
+    private void PlayerMovementAnimation(float speed)
     {
         playerAnimator.SetFloat("Speed", Mathf.Abs(speed));
         Vector3 scale = transform.localScale;
