2cfb9811fd62217a16e9ab576314fac8b6adb50d
SrinivasPrasad <43960959+SrinuSandy@users.noreply.github.com>
2025-06-10 16:08:22 +0530

    implement if the player is grounded or not before player jump

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,6 +6,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 10f;
     [SerializeField] private Rigidbody2D playerRB;
     [SerializeField] private float forceSpeed;
+
+    [SerializeField] private bool isGrounded = false;
 
     private void Start()
     {
@@ -19,29 +18,29 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float vertical = Input.GetAxisRaw("Vertical");
 
 
         PlayerMovement(horizontal, vertical);
         PlayerMovementAnimation(horizontal);
         Crouch(Input.GetKey(KeyCode.LeftControl));
-        Jump(vertical > 0);
+        JumpAnimation(vertical > 0);
     }
 
     private void PlayerMovement(float horizontal, float vertical) 
     {
-        // player Movement
+        if (!isGrounded) return;
+       
         Vector3 playerPos = transform.position;
         playerPos.x += horizontal * speed * Time.deltaTime;
         transform.position = playerPos;
 
-        // Player Jump
-        playerRB.AddForce(new Vector2(0, vertical * forceSpeed), ForceMode2D.Force);
-
+        playerRB.AddForce(new Vector2(0, vertical * forceSpeed), ForceMode2D.Impulse);
     }
 
     private void PlayerMovementAnimation(float speed)
     {
+        if (!isGrounded) return;
         playerAnimator.SetFloat("Speed", Mathf.Abs(speed));
         Vector3 scale = transform.localScale;
         if (speed < 0f)
@@ -60,8 +59,29 @@ public class PlayerController : MonoBehaviour
         playerAnimator.SetBool("Crouch", isCrouch);
     }
 
-    private void Jump(bool isJump)
+    private void JumpAnimation(bool isJump)
     {
+        if (!isGrounded) return;
         playerAnimator.SetBool("Jump", isJump);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        CheckGround(collision, true);
+    }
+
+    
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        CheckGround(collision, false);
+    }
+
+    private void CheckGround(Collision2D collision, bool grounded)
+    {
+        if (collision.transform.CompareTag("Ground"))
+        {
+            isGrounded = grounded;
+        }
     }
 }
